8a46f00ce6fb97dc46c9772d3ccf42e79b7aca44
cwaldock1 <conorwaldock@gmail.com>
2023-11-06 10:01:24 +0100

    update bash script to work on server

--- scripts/sbatch_runShapley_ubelix.sh
+++ scripts/sbatch_runShapley_ubelix.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 #SBATCH --mail-user=conor.waldock@unibe.ch
 #SBATCH --mail-type=end,fail,begin
-#SBATCH --time=2-00:00:00
-#SBATCH --mem-per-cpu=64G
-#SBATCH --cpus-per-task=5
+#SBATCH --time=4-00:00:00
+#SBATCH --mem-per-cpu=128G
+#SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
-#SBATCH --tmp=64G
+#SBATCH --tmp=128G
 #SBATCH --array=1-9
 #SBATCH --output='error_shapley/slurm-%j.out'
 #SBATCH --error='output_shapley/slurm-%j.out'
